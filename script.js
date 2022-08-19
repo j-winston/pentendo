11891e4d3ea5437534d04260d1af872579706d2d
j-winston <kenjameswinston@gmail.com>
2022-08-18 20:26:25 -0400

    commit

--- script.js
+++ script.js
@@ -1,42 +1,39 @@
-
-// Draws a grid of divs within .main-container
+// Draws a grid of divs within .main-container testing comment
 function drawGrid(containerWidth, containerHeight, gridSize) {
-    
-    let numRows = gridSize;
-    let numCells = numRows; // if there's 3 rows, there's 3 cells per row
-    let cellSize = (containerWidth / numCells) - 2; // Account for border!
+  let numRows = gridSize;
+  let numCells = numRows; // if there's 3 rows, there's 3 cells per row
+  let cellSize = containerWidth / numCells - 2; // Account for border!
 
-    
-    let mainContainerEl = document.querySelector('.grid-container');
-    containerWidth = containerWidth.toString();
-    containerHeight = containerHeight.toString();
+  let mainContainerEl = document.querySelector(".grid-container");
+  containerWidth = containerWidth.toString();
+  containerHeight = containerHeight.toString();
 
-    mainContainerEl.style.width = containerWidth + 'px';
-    mainContainerEl.style.height = containerHeight + 'px';
+  mainContainerEl.style.width = containerWidth + "px";
+  mainContainerEl.style.height = containerHeight + "px";
 
-    for (x=0; x<numRows; x++) {
-        // Create each column
-        let rowEl = document.createElement('div');
-        rowEl.classList.add('row');
+  for (x = 0; x < numRows; x++) {
+    // Create each column
+    let rowEl = document.createElement("div");
+    rowEl.classList.add("row");
 
-        for(i=0; i<numCells; i++){
-            // Fill each column with cells across
-            let cellEl = document.createElement('div');
-            cellEl.classList.add('cell');
-            
-            // // Set height and width of each cell
-            height = cellSize.toString();
-            width = cellSize.toString();
-            
-            cellEl.style.height = height + 'px';
-            cellEl.style.width = width + 'px';
+    for (i = 0; i < numCells; i++) {
+      // Fill each column with cells across
+      let cellEl = document.createElement("div");
+      cellEl.classList.add("cell");
 
-            // Add cell to column
-            rowEl.appendChild(cellEl);
-        }
-        // Add column to main-container
-        mainContainerEl.appendChild(rowEl);
+      // // Set height and width of each cell
+      height = cellSize.toString();
+      width = cellSize.toString();
+
+      cellEl.style.height = height + "px";
+      cellEl.style.width = width + "px";
+
+      // Add cell to column
+      rowEl.appendChild(cellEl);
     }
+    // Add column to main-container
+    mainContainerEl.appendChild(rowEl);
+  }
 }
 
 drawGrid(500, 500, [5]);
