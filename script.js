b4031b98116b122d39927dde990d89a70fbae981
j-winston <kenjameswinston@gmail.com>
2022-08-24 17:50:09 -0400

    add grid line toggle functionality

--- script.js
+++ script.js
@@ -1,8 +1,5 @@
 // Draws a grid of divs within .main-container testing comment
 function drawGrid(containerWidth, containerHeight, gridSize) {
-
-  
-
   let numRows = gridSize;
   let numCells = numRows; // if there's 3 rows, there's 3 cells per row
   let cellSize = containerWidth / numCells - 2; // Account for border!
@@ -38,17 +35,19 @@ function drawGrid(containerWidth, containerHeight, gridSize) {
     // Add column to main-container
   
     mainContainerEl.appendChild(rowEl);
+
   }
 }
+ 
 
-function clearGrid(){
+function clearGrid() {
   const allRows = document.querySelectorAll('.row');
   allRows.forEach(row => row.remove());
 
 }
 
 
-function setGridSize(){
+function setGridSize() {
   gridSize = Number(this.value);
   clearGrid();
   // update grid size indicator
@@ -57,19 +56,56 @@ function setGridSize(){
  
 }
 
-function updateGridDisplay(gridSize){
+
+function updateGridDisplay(gridSize) {
   let bigDigit = document.querySelector('.high-digit');
   let smallDigit = document.querySelector('.low-digit');
 
   bigDigit.textContent = gridSize;
   smallDigit.textContent = 'x' + gridSize;
 
+}
+
+
+function toggleGridLines() {
+  // If grid is showing turn it off
+  if (showingGrid) {
+    const cells = document.querySelectorAll('.cell');
+    cells.forEach(cell => cell.style.border = "1px solid #eeeeee");
+    // Set latch for next time around
+    showingGrid = false;
+    // change the icon
+    document.getElementById('grid-toggle-icon').src = "assets/grid-off.png";
+    
+
+
+
+  }else {
+    showGridLines();
+    showingGrid = true;
+    document.getElementById('grid-toggle-icon').src = "assets/grid-on.png";
+  }
+}
+
+
+function showGridLines() {
+  const cells = document.querySelectorAll('.cell');
+  cells.forEach(cell => cell.style.border = "1px solid #d5d4d4");
 
 }
 
 
+// MAIN 
+let showingGrid = true;
+
 const rangeInput = document.querySelector('.slider');
 rangeInput.addEventListener('input', setGridSize)
+
+const gridToggler = document.querySelector(".grid-toggle-container");
+gridToggler.addEventListener('click', toggleGridLines);
+
+
+
 
 // // Draw default grid 
 drawGrid(700,700, 2);
